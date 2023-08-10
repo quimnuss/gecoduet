ee4c2acbdd2535799590f8fd2cc25b901b05b573
quimnuss <quimnuss@petit>
2023-08-10 20:10:47 +0200

    add resources and art

---
+++ data/animal_base_resource.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name AnimalResource
+
+
+@export var mobility_type : String = "run"
+@export var animation_frames : SpriteFrames
+@export var name : String
+@export var species : String
+@export var scale : float = 1
---
+++ data/elder_base_resource.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name ElderResource
+
+@export var initial_density: float
+@export var mutuality : Dictionary
+@export var species : String
+@export var subspecies : String
