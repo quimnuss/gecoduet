715b8db0a0e146fdb59dd0cc1430419c4992833c
quimnuss <quimnuss@petit>
2023-08-13 12:22:57 +0200

    elder totem with spawns

--- actors/ecosystem.gd
+++ actors/ecosystem.gd
@@ -6,8 +6,13 @@ var res_bear = preload("res://data/animal_bear.tres")
 var res_mutuality = preload("res://data/elder_glv.tres")
 
 var res_elder = preload("res://actors/elder.tscn")
+
+var totem = preload("res://actors/elder_totem.tscn")
+
 var elders = []
 var density_map : Dictionary = {}
+
+@export var totem_spawns = []
 
 @onready var glv_timer = $Timer
 #@onready var sample_elder = $sample_elder
@@ -17,7 +22,13 @@ func _ready():
     # editor instanced elder
 #    add_elder(sample_elder)
 
+    # reparent totem spawns to be part of the ecosystem # should we do this?
+    totem_spawns = get_tree().get_nodes_in_group("totem_spawns")
+    for totem_spawn in totem_spawns:
+        add_child(totem_spawn)
+
     # script instanced elder
+    spawn_totem("carrot")
     spawn_elder("carrot")
     spawn_elder("bear")
     spawn_elder("rabbit")
@@ -29,6 +40,11 @@ func spawn_elder(elder_name : String, spawn_position : Vector2 = Vector2.ZERO):
     elder.mutuality = res_mutuality.get_data()[elder_name]
     add_elder(elder)
     return elder
+
+func spawn_totem(elder_name:String):
+    var totem_spawn = totem_spawns.pop_back()
+    var new_totem = totem.instantiate()
+    totem_spawn.add_child(new_totem)
 
 func add_elder(elder):
     elders.append(elder)
