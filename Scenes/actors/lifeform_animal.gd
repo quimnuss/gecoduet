041708de25c9fd118b9172e7ff6eb02138228839
quimnuss <lluna.nova@gmail.com>
2023-09-02 02:30:23 +0200

    add state machine and movement

--- Scenes/actors/lifeform_animal.gd
+++ Scenes/actors/lifeform_animal.gd
@@ -9,12 +9,18 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var resource : LifeformAnimalResource = preload("res://data/lifeform_bear.tres")
 
 @onready var animation = $AnimatedSprite2D
+@onready var state_machine = $StateMachinePlayer
+@onready var movement = $Movement
+
+var debug = false
 
 func _ready():
 	self.animation.set_sprite_frames(resource.animation_frames)
 	self.name = resource.name
 	self.set_scale(Vector2(resource.scale,resource.scale))
 	prints("Animal ready")
+	movement.pawn = self
+	animation.pawn = self
 	
 
 func _physics_process(delta):
@@ -28,4 +34,6 @@ func _physics_process(delta):
 	else:
 		velocity = velocity.move_toward(Vector2(0,0), speed/10)
 
+	state_machine.set_param("velocity", velocity.length_squared())
+	prints("velocity squared",velocity.length_squared())
 	move_and_slide()
