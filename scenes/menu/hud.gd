7a801ab12c56990bb52b614d527b8fe51e972552
quimnuss <lluna.nova@gmail.com>
2023-09-05 22:51:56 +0200

    disable other buttons, but show them

--- scenes/menu/hud.gd
+++ scenes/menu/hud.gd
@@ -1,39 +1,39 @@
 extends Control
 
 var emoji_to_species : Dictionary = {
-    '🐺': Constants.Species.BEAR,
-    '🐇': Constants.Species.RABBIT,
-    '🥕': Constants.Species.CARROT,
-    '🦊': Constants.Species.TREE,
-    '🐦': Constants.Species.TREE,
-    '🐝': Constants.Species.TREE,
-    '🐛': Constants.Species.TREE,
-    '🐟': Constants.Species.TREE,
-    '🌱': Constants.Species.TREE,
-    '🍒': Constants.Species.TREE
+	'🐺': Constants.Species.BEAR,
+	'🐇': Constants.Species.RABBIT,
+	'🥕': Constants.Species.CARROT,
+	'🦊': Constants.Species.TREE,
+	'🐦': Constants.Species.TREE,
+	'🐝': Constants.Species.TREE,
+	'🐛': Constants.Species.TREE,
+	'🐟': Constants.Species.TREE,
+	'🌱': Constants.Species.TREE,
+	'🍒': Constants.Species.TREE
 }
 
 signal create_species(lifeform : Constants.Species)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
 
 func _on_species_button_pressed(caller: Button):
-    prints("pressed",caller)
-    var species_emoji = caller.get_text()
-    var species = emoji_to_species[species_emoji]
-    create_species.emit(species)
+	prints("pressed",caller)
+	var species_emoji = caller.get_text()
+	var species = emoji_to_species[species_emoji]
+	create_species.emit(species)
 
 func _on_species_name_button_pressed(name : String):
-    var species = Constants.Species.get(name.to_upper())
-    if species != null:
-        create_species.emit(species)
-    else:
-        prints("species",name.to_upper(),"unknown. Species:",Constants.Species.keys())
+	var species = Constants.Species.get(name.to_upper())
+	if species != null:
+		create_species.emit(species)
+	else:
+		prints("species",name.to_upper(),"unknown. Species:",Constants.Species.keys())
 
